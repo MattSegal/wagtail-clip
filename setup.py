6bdb280d125a2a4dd78a65b1c19e38744f6f8418
Matthew Segal <mattdsegal@gmail.com>
2021-09-30 17:00:51 +1000

    Cheat

--- setup.py
+++ setup.py
@@ -20,8 +20,6 @@ setup(
     license="MIT",
     install_requires=[
         "wagtail>=2",
-        "torch==1.7.1",
-        "torchvision==0.8.2",
         "clip@git+https://github.com/openai/CLIP.git",
     ],
     # https://pypi.org/pypi?%3Aaction=list_classifiers
